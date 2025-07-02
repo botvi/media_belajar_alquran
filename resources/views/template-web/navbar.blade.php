ee55e746b3b5a2e5203dace0706342eec41e82d0
Yovi Ardiansyah <younoob07mei@gmail.com>
2025-07-02 09:20:13 +0700

    main

--- resources/views/template-web/navbar.blade.php
+++ resources/views/template-web/navbar.blade.php
@@ -5,9 +5,9 @@
     <nav id="navmenu" class="navmenu">
       <ul>
         <li><a href="/" class="{{ request()->is('/') ? 'active' : '' }}"><i class="bi bi-house navicon"></i><span>Home</span></a></li>
+        <li><a href="/materi" class="{{ request()->is('materi*') ? 'active' : '' }}"><i class="bi bi-journal-text navicon"></i><span>Materi</span></a></li>
         <li><a href="/surah" class="{{ request()->is('surah*') ? 'active' : '' }}"><i class="bi bi-book navicon"></i><span>Surah</span></a></li>
         <li><a href="/sunnah" class="{{ request()->is('sunnah*') ? 'active' : '' }}"><i class="bi bi-bookmark navicon"></i><span>Sunnah</span></a></li>
-        <li><a href="/materi" class="{{ request()->is('materi*') ? 'active' : '' }}"><i class="bi bi-journal-text navicon"></i><span>Materi</span></a></li>
         <li><a href="/quiz" class="{{ request()->is('quiz*') ? 'active' : '' }}"><i class="bi bi-question-circle navicon"></i><span>Quiz</span></a></li>
       </ul>
     </nav>
